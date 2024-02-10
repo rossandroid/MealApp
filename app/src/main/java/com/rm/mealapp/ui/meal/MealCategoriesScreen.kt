8c17a21b05ff79eca16933734b678af18c762b7d
Rossella Morgante <>
2024-02-10 13:53:23 +0000

    add description meal on the card

--- app/src/main/java/com/rm/mealapp/ui/meal/MealCategoriesScreen.kt
+++ app/src/main/java/com/rm/mealapp/ui/meal/MealCategoriesScreen.kt
@@ -1,6 +1,7 @@
 package com.rm.mealapp.ui.meal
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,17 +11,28 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.rm.mealapp.ui.theme.MealAppTheme
 import com.rm.model.response.MealResponse
@@ -40,27 +52,55 @@ fun MealCategoriesScreen() {
 }
 @Composable
 fun MealCategory(meal: MealResponse){
-    ElevatedCard(shape = RoundedCornerShape(8.dp),
+    var isExpanded by remember { mutableStateOf(false) }
+    ElevatedCard(
+        shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 2.dp
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 16.dp)) {
+            .padding(top = 16.dp)
+    ) {
         Row {
-            Image(painter = rememberAsyncImagePainter(model = meal.imageUrl),
+            Image(
+                painter = rememberAsyncImagePainter(model = meal.imageUrl),
                 contentDescription = null,
                 modifier = Modifier
                     .size(88.dp)
-                    .padding(4.dp))
-            Column(modifier = Modifier
-                .align(Alignment.CenterVertically)
-                .padding(16.dp)) {
-                Text(text = meal.name,
-                    style = MaterialTheme.typography.bodyMedium)
-            }
-        }
+                    .padding(4.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = meal.name,
+                    style = MaterialTheme.typography.bodyMedium
+                )
 
+                CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurface) {
+                Text(text = meal.description,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.bodyMedium,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = if (isExpanded) 10 else 4)
+                }
+            }
+            Icon(
+                imageVector = if(isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(if (isExpanded)
+                        Alignment.Bottom
+                    else
+                        Alignment.CenterVertically)
+                    .clickable { isExpanded = !isExpanded }
+            )
+        }
     }
 
 }
